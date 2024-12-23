992ffbde60f28fa5c050d936c9a4daf778793827
Ben LAM <lamben2007@gmail.com>
2024-12-23 16:34:59 +0100

    ajout composant collapse + ajout page about + modif page home + modif composant banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,37 @@
 import './Banner.scss'
+import PropTypes from 'prop-types';
 
-function Banner() {
+function Banner({ type }) {
+
+
+  //
   return (
 
     <div className="divBanner">
 
       <div className="divBanner_overlay"></div>
       <div className="divBanner_content">
-        <span>Chez vous, partout et ailleurs</span>
+
+        {(type === "home" ? (<span>Chez vous, partout et ailleurs</span>) : (<></>))}
+
       </div>
+
+
+      <img
+        src={type === "home" ? "public/bannerHome.png" : "public/bannerAbout.webp"}
+        alt={type === "home" ? "Accueil" : "A propos"}
+        className="divBanner_image"
+      />
 
     </div>
 
   )
 
+}
+
+
+Banner.propTypes = {
+  type: PropTypes.string.isRequired
 }
 
 export default Banner
--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,16 +1,46 @@
+import { useState } from 'react';
 import './Collapse.scss'
+import PropTypes from 'prop-types';
 
-function Collapse() {
+function Collapse({title, collapseText}) {
+
+    //
+    const [isOpen, setIsOpen] = useState(false);
+
+    //
+    const handleClick = () => {
+        //
+        setIsOpen(!isOpen);
+    };
+
+
+    //
     return (
-        <>
 
-            <div className="divCollapse">
-                COLLAPSE
+        <div className="divCollapse">
+
+            <div className="divCollapse_bar">
+                <div className="divCollapse_bar_title">{title}</div>
+
+                <button className="divCollaps_bar_button" onClick={handleClick}>
+                    <img className={isOpen ? 'rotated' : ''} src="public/topArrow.png" alt="arrow" />
+                </button>
             </div>
 
-        </>
+            <div className={`divCollapse_text ${!isOpen ? 'divCollapse_text--invisible' : ''}`}>
+                {collapseText}
+            </div>
+
+        </div>
+
     )
 
 }
+
+//
+Collapse.propTypes = {
+    title: PropTypes.string.isRequired,
+    collapseText: PropTypes.string.isRequired,
+};
 
 export default Collapse
